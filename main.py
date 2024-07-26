f13fd9b8f81054790385b3d298c2e63068e09cc6
Soultan <soultntoure@gmail.com>
2024-07-26 13:44:24 +0300

    first time screaping live web, now it prints the article with most upvotes and its link

--- main.py
+++ main.py
@@ -2,14 +2,22 @@ from bs4 import BeautifulSoup
 import requests
 
 
-response = requests.get("https://appbrewery.github.io/news.ycombinator.com/")
+response = requests.get("https://news.ycombinator.com/news")
 yc_web_page = response.text
 
-soup = BeautifulSoup(yc_web_page, "html.parser")
-titles = soup.select(selector=".storylink")
-count = 1
-for title in titles:
-    print(f"{count}-{title.text}")
-    count +=1
+soup = BeautifulSoup(yc_web_page, 'html.parser')
+instance = soup.find(name="span", class_="titleline")
 
+articles = soup.find_all(name="span", class_="titleline")
+article_texts = [article_tag.getText() for article_tag in articles]
+article_links = [article_tag.find(name='a').get("href") for article_tag in articles]
+article_upvotes = [int(score.getText().split()[0]) for score in soup.find_all(name="span", class_="score")]
 
+largest_number = max(article_upvotes)
+largest_index = article_upvotes.index(largest_number)
+
+print(
+    f"Most upvoted article: {article_texts[largest_index]}\n"
+    f"Number of upvotes: {article_upvotes[largest_index]} points\n"
+    f"Available at: {article_links[largest_index]}."
+)
